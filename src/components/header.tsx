40d897b818a0735ad0190c4c505b95689044ff74
Maxime Sickenberg <maxime.sickenberg@gmail.com>
2025-02-27 11:07:25 +0100

    Bump version to 1.0.5

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -6,10 +6,10 @@ export default function Header() {
         <header className="sticky top-0 inset-x-0 z-40 bg-white flex items-center justify-between border-b border-zinc-300 h-14 px-4 sm:px-10 lg:px-16">
             <div className="flex items-end gap-2">
                 <Link href="/" className="font-bold">
-                    <span>PMI - CopyPasta</span>
+                    <span>CopyPasta</span>
                 </Link>
                 <span className="inline-flex items-center rounded-md bg-cyan-50 px-2 py-1 text-xs font-medium text-cyan-600 ring-1 ring-inset ring-cyan-500/10">
-                    V.1.0.4
+                    V.1.0.5
                 </span>
             </div>
 
